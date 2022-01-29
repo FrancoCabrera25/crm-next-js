61e516ae61f1180206ce05e953a1960d30d352a6
Franco Cabrera <cabrera.franco@outlook.com>
2022-01-29 15:34:12 -0300

    initi commit

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <h1> desde Layout</h1>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+const  Home = () => {
+return(
+  <div>
+      <Layout>
+        <h2>desde home</h2>
+      </Layout>
+  </div>
+)
+}
+
+export default Home
