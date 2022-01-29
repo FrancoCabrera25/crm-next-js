3a569e3450140d8d591d7ab6c394156dffa07a96
Franco Cabrera <cabrera.franco@outlook.com>
2022-01-29 16:04:49 -0300

    init sidenav

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,26 @@
-import react from "react";
+import Head from "next/head";
+import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <h1> desde Layout</h1>
-      {children}
+      <Head>
+        <title>CRM</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+          integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+      <div className=" bg-gray-200 min-h-screem">
+        <div className="flex min-h-screen">
+          <Sidebar />
+
+          {children}
+        </div>
+      </div>
     </>
   );
 };
