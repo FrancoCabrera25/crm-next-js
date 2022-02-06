6ab9d9d04e45c3caa07462005edced7de7e75a41
Franco Cabrera <cabrera.franco@outlook.com>
2022-02-06 16:29:39 -0300

    add apollo

--- pages/registro.js
+++ pages/registro.js
@@ -1,35 +1,45 @@
-
 import Layout from "../components/Layout";
 
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Registro = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+               .required("El nombre es obligatorio"),
+      lastname: Yup.string()
+                   .required("El apellido es obligatorio"),
+      email: Yup.string()
+                .email("El email es invalido")
+                .required("El email es obligatorio"),
+      password: Yup.string()
+                   .required("La contraseña es obligatoria")
+                   .min(6, "La contraseña deber contener al menos 6 caracteres"),
+    }),
+    onSubmit: (data) => {
+      console.log("data", data);
+    },
+  });
 
-    const formik = useFormik({
-        initialValues: {
-            name: '',
-            lastname: '',
-            email: '',
-            password: ''
-        },
-        validationSchema: Yup.object({
-            name: Yup.string().required('El nombre es obligatorio'),
-        }),
-        onSubmit: data =>{
-            console.log('data', data);
-        }
-    });
-
-
-    return(
-        <Layout>
-      <h1 className="text-center text-2xl text-white font-light">Registrarse</h1>
+  return (
+    <Layout>
+      <h1 className="text-center text-2xl text-white font-light">
+        Registrarse
+      </h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4" onSubmit={formik.handleSubmit}>
-
-          <div className="mb-4">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="name"
@@ -37,9 +47,9 @@ const Registro = () => {
                 Nombre
               </label>
               <input
-              className={`${"shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"}  ${
-                formik.errors.name ? 'border-red-500' : ''
-              }`}
+                className={`${"shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"}  ${
+                  formik.errors.name ? "border-red-500" : ""
+                }`}
                 id="name"
                 type="text"
                 placeholder="Ingrese nombre"
@@ -47,12 +57,12 @@ const Registro = () => {
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
-            </div> 
+            </div>
             {formik.errors.name ? (
-                <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2 text-sm">
-                    <p>{formik.errors.name}</p>
-                </div>
-            ) : null }
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2 text-sm">
+                <p>{formik.errors.name}</p>
+              </div>
+            ) : null}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -61,14 +71,21 @@ const Registro = () => {
                 Apellido
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"
-                id="lastname"
+                className={`${"shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"}  ${
+                  formik.errors.lastname ? "border-red-500" : ""
+                }`}
                 type="text"
                 placeholder="Ingrese apellido"
                 value={formik.values.lastname}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
-            </div> 
+            </div>
+            {formik.errors.lastname ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2 text-sm">
+                <p>{formik.errors.lastname}</p>
+              </div>
+            ) : null}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -77,14 +94,22 @@ const Registro = () => {
                 Email
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"
+                   className={`${"shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"}  ${
+                    formik.errors.email ? "border-red-500" : ""
+                  }`}
                 id="email"
                 type="email"
                 placeholder="Ingrese Email"
                 value={formik.values.email}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.errors.email ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2 text-sm">
+                <p>{formik.errors.email}</p>
+              </div>
+            ) : null}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -93,23 +118,32 @@ const Registro = () => {
                 Contraseña
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"
+                  className={`${"shadow appearance-none border rounded w-full py-2 px-2 text-gray-700 leading-tigh focus:outline-none focus:shadow-outline"}  ${
+                   formik.errors.password ? "border-red-500" : ""
+                 }`}
                 id="password"
                 type="password"
                 placeholder="Ingrese contraseña"
                 value={formik.values.password}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
-            <input type="submit"    
-             className="bg-gray-700 w-full mt-5 p-2 text-white uppercase hover:cursor-pointer hover:bg-gray-900"
-             value="Crear cuenta"
+            {formik.errors.password ? (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-2 text-sm">
+                <p>{formik.errors.password}</p>
+              </div>
+            ) : null}
+            <input
+              type="submit"
+              className="bg-gray-700 w-full mt-5 p-2 text-white uppercase hover:cursor-pointer hover:bg-gray-900"
+              value="Crear cuenta"
             />
           </form>
         </div>
       </div>
     </Layout>
-    );
-}
+  );
+};
 
 export default Registro;
