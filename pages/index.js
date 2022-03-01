c021f682d33744cb729005513b35ded5c8517f50
Franco Cabrera <64378629+FrancoCabrera25@users.noreply.github.com>
2022-02-28 22:36:37 -0300

    show client

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,57 @@
-import Layout from "../components/Layout"
-const  Home = () => {
-return(
-  <div>
+import Layout from "../components/Layout";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_CLIENT = gql`
+  query ExampleQuery {
+    getClients {
+      id
+      name
+      lastname
+      company
+      email
+      phone
+      dni
+      seller
+    }
+  }
+`;
+
+const Home = () => {
+  const { data, loading, error } = useQuery(GET_CLIENT);
+  // console.log(data);
+  // console.log(loading);
+  // console.log(error);
+
+  if (loading) return "Cargando";
+  return (
+    <div>
       <Layout>
         <h1 className="text-2xl text-gray-800 font-ligth">Clientes</h1>
-      </Layout>
-  </div>
-)
-}
 
-export default Home
+        <table className="table-auto shadow-md mt-10 w-full w-lg">
+          <thead className="bg-gray-800">
+            <tr className="text-white">
+              <th className="w-1/5 py-2">Nombre</th>
+              <th className="w-1/5 py-2">Empresa</th>
+              <th className="w-1/5 py-2">Email</th>
+            </tr>
+          </thead>
+          <tbody className="bg-white">
+            {data.getClients.map((client) => (
+              <tr key={client.id}>
+                <td className="border px-4 py-2">
+                  {" "}
+                  {client.name} {client.lastname}
+                </td>
+                <td className="border px-4 py-2"> {client.company}</td>
+                <td className="border px-4 py-2"> {client.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Layout>
+    </div>
+  );
+};
+
+export default Home;
